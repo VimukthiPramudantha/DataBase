180f60375359501479a6eb9293402f90a66c7aa5
Vimukthi pramudantha <vimukthipramudanth@gmail.com>
2025-01-29 21:11:32 +0530

    get info

--- BACKEND/routes/students.js
+++ BACKEND/routes/students.js
@@ -4,7 +4,32 @@ let Student = require("../models/student");
 router.route("/add").POST((req,res)=>{
 
     const name = req.body.name;
-    const age = req.body.age;
+    const age = Number(req.body.age);
     const gender = req.body.gender;
     const address = req.body.address;
+
+    const newStudent = new Student({
+        name,
+        age,
+        gender,
+        address
+    })
+
+    newStudent.save().then(() =>{
+        res.json("Student added")
+    }).catch(()=>{
+        console.log(err);
+    })
 })
+
+
+router.route("/").GET((req,res)=>{
+    Student.find().then((students)=>{
+        res.json(students)
+    }).catch((err)=>{
+        console.log(err)
+    })
+}) 
+
+
+module.exports = router;
