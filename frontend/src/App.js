8f1b7ab3a5ba2598bfcadd4be111ab6eb665d945
Vimukthi pramudantha <vimukthipramudanth@gmail.com>
2025-02-04 20:18:28 +0530

    initialize frontend structure with React components and configuration files

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import CounterClass from './components/CounterClass';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <CounterClass/>
+    </div>
+  );
+}
+
+export default App;
